528d59b8f7e1880a2b52310bdc700662d5f801f0
miquellopez97 <miquel.lopez97@gmail.com>
2022-06-23 13:40:22 +0200

    Add rookies

--- src/controllers/google.controller.js
+++ src/controllers/google.controller.js
@@ -5,21 +5,20 @@ photoPlayers = {
     4 : 'https://s3-eu-west-1.amazonaws.com/playofffederacions/basquet/FED_FOTO/Thumbs/PER_0ace4ac21-cfc1-4347-a379-ac438f4d14fc.jpg',
     5 : 'https://s3-eu-west-1.amazonaws.com/playofffederacions/basquet/FED_FOTO/Thumbs/PER_0f23e5f32-2ca6-454d-85de-4b51838f3f5b.jpg',
     7 : 'https://s3-eu-west-1.amazonaws.com/playofffederacions/basquet/FED_FOTO/Thumbs/PER_0a367652b-1347-452e-8901-5c47df4f64bc.jpeg',
-    8 : 'https://s3-eu-west-1.amazonaws.com/playofffederacions/basquet/FED_FOTO/Thumbs/PER_0b8255b1d-0af2-42c8-8ac7-b3c39cfd6c2e.jpg',
+    8 : 'https://d1wppq8sjja81z.cloudfront.net/basquet/FED_FOTO/Thumbs/PER_355832_6dc9ba60-fe11-4fed-ba8b-0474bb3c8fee.jpg',
     9 : 'https://s3-eu-west-1.amazonaws.com/playofffederacions/basquet/FED_FOTO/Thumbs/PER_0c0bb3e2d-2aa1-4ff6-95cb-cf6d8847a09a.jpg',
     10 : 'https://s3-eu-west-1.amazonaws.com/playofffederacions/basquet/FED_FOTO/Thumbs/PER_05cece0da-51f9-4307-844d-858975439077.png',
     11 : 'https://s3-eu-west-1.amazonaws.com/playofffederacions/basquet/FED_FOTO/Thumbs/PER_06dec579f-b152-42b6-86f8-ad4cf83d9f64.png',
     14 : 'https://s3-eu-west-1.amazonaws.com/playofffederacions/basquet/FED_FOTO/Thumbs/PER_0df8da245-4747-489b-8832-dc1634d34156.png',
-    15 : 'https://s3-eu-west-1.amazonaws.com/playofffederacions/basquet/FED_FOTO/Thumbs/PER_0227aed8a-1c0a-410a-8004-d605e23a54ae.jpg',
     18 : 'https://s3-eu-west-1.amazonaws.com/playofffederacions/basquet/FED_FOTO/Thumbs/PER_052d9b5c9-0be5-47be-b002-850258b7f185.png',
-    24 : 'https://s3-eu-west-1.amazonaws.com/playofffederacions/basquet/FED_FOTO/Thumbs/PER_097f226e4-6bcf-4799-8437-3f16ec9b4333.jpg',
-    99: 'https://d1wppq8sjja81z.cloudfront.net/basquet/FED_FOTO/Thumbs/PER_381471_8083ec1a-d815-4aaa-b86e-6a9de070c1de.jpg'
+    21 : 'https://d1wppq8sjja81z.cloudfront.net/basquet/FED_FOTO/Thumbs/PER_353164_5c96e464-d786-4845-be85-d9de9e27e739.jpg',
+    24: 'https://d1wppq8sjja81z.cloudfront.net/basquet/FED_FOTO/Thumbs/PER_381471_8083ec1a-d815-4aaa-b86e-6a9de070c1de.jpg'
 };
 
 const obtenerDatos = async (req, res) => {
     rowData = await googleSheet.accessGoogleSheet();
     const currentDate = new Date();
-    showplayer = [rowData[0]];
+    showplayer = [rowData[1]];
     players = [];
     critical = [];
     twoWeeks = [];
